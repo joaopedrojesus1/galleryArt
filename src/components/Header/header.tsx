986c828752bbb5276ab3a704758311650dca6f49
AnaByo <>
2023-10-30 21:54:21 -0300

    feat:inclusion of header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,9 @@
+// const links = ["Home", "Art", "About", "Blog"];
+
+export default function Header(){
+    return (
+        <nav className = "bg-gray-800 flex justify-between items-center h-20 p-4 ">
+            <ul></ul>
+        </nav>
+    )
+}
