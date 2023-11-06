7c01cc9a2743cd8d5074ecd6f317bfd185cfb498
Joao Pedro Neris <114251012+joaopedrojesus1@users.noreply.github.com>
2023-11-06 19:26:18 -0300

    Delete src directory

--- src/components/Header/header.tsx
+++
@@ -1,9 +0,0 @@
-// const links = ["Home", "Art", "About", "Blog"];
-
-export default function Header(){
-    return (
-        <nav className = "bg-gray-800 flex justify-between p-10">
-            <ul></ul>
-        </nav>
-    )
-}
